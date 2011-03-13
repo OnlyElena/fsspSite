877a502ead69c6a8cfd5f4c5970d596264f9c7ce
OnlyElena <OnlyElena@mail.ru>
2011-03-13 13:46:20 +0800

    initial commit

---
+++ src/main/java/ru/fssp38/fsspSite/App.java
@@ -0,0 +1,13 @@
+package ru.fssp38.fsspSite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

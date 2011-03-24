1184fea6367979fab09ae95dbe02ee3557e72991
OnlyElena <web@fssp38.ru>
2011-03-24 18:39:45 +0800

    добавлен стартовый дизайн

--- src/main/java/ru/fssp38/fsspSite/App.java
+++ src/main/java/ru/fssp38/fsspSite/App.java
@@ -1,13 +1,38 @@
 package ru.fssp38.fsspSite;
 
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
 /**
- * Hello world!
+ * Стартовая страница
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends SingleFrameApplication {
+
+        @Override protected void startup() {
+            show(new FsspSiteView(this));
+        }
+
+        /**
+         * This method is to initialize the specified window by injecting resources.
+         * Windows shown in our application come fully initialized from the GUI
+         * builder, so this additional configuration is not needed.
+         */
+        @Override protected void configureWindow(java.awt.Window root) {
+        }
+
+        /**
+         * A convenient static getter for the application instance.
+         * @return the instance of App
+         */
+        public static App getApplication() {
+            return Application.getInstance(App.class);
+        }
+
+        /**
+         * Main method launching the application.
+         */
+        public static void main(String[] args) {
+            launch(App.class, args);
+        }
     }
-}
